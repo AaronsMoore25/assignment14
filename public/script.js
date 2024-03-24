c898081e16a6e45435d3e1ac5b0e5af067d4dfaf
AaronSMoore2025 <aaron.moore2026@gmail.com>
2024-03-24 14:00:30 -0400

    changes

--- public/script.js
+++ public/script.js
@@ -10,7 +10,6 @@ const getCrafts = async() => {
 };
 
 const showCrafts = async() => {
-    console.log("i am in here");
     const crafts = await getCrafts();
     crafts.forEach((craft) => {
         const section = getCraftSection(craft);
@@ -26,6 +25,44 @@ const getCraftSection = (craft) => {
     const img = document.createElement("img");
     img.src="http://localhost:3000/images/" + craft.image;
     mainSection.append(img);
+
+    mainSection.onclick = (e) => {
+        console.log("the onclick is working");
+        document.getElementById("dialog").style.display = "block";
+
+        const details = document.getElementById("dialog-details");
+        details.innerHTML = "";
+        const imageDetails = document.getElementById("dialog-image");
+        imageDetails.innerHTML = "";
+
+        const myImage = document.createElement("img");
+        myImage.src = "";
+        myImage.src = "http://localhost:3000/images/" + craft.image;
+        myImage.id = "myImage";
+        imageDetails.append(myImage);
+        console.log(myImage);
+
+        const myName = document.createElement("h1");
+        myName.innerHTML = craft.name;
+        details.append(myName);
+        console.log(myName);
+
+        const myDescription = document.createElement("p");
+        myDescription.innerHTML = craft.description;
+        details.append(myDescription);
+        console.log(myDescription);
+
+        const mySupplies = document.createElement("p");
+        mySupplies.innerHTML = "Supplies: " + craft.supplies;
+        details.append(mySupplies);
+        console.log(mySupplies);
+        
+    };
+
+    document.getElementById("dialog-close").onclick = () => {
+        document.getElementById("dialog").style.display = "none";
+    };
+
     return mainSection;
 }
 
